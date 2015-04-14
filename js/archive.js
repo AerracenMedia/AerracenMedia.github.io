d85f2bdbafddfd2cc9f8407dc9d609cd76876f03
spleen387 <spleen_387@me.com>
2015-04-14 13:37:19 -0600

    wonky
    It looks a little ridiculous, but I think you can fix it with some css

---
+++ js/archive.js
@@ -0,0 +1,58 @@
+(function(){
+  "use strict";
+
+  ([
+    {
+      link:"http://google.com",
+      title:"Google",
+      description:"search engine extrordinaire."
+    },
+    {
+      link:"http://yahoo.com",
+      title:"Yahoo",
+      description:"search engine with amazing QA site."
+    },
+    {
+      link:"http://google.com",
+      title:"Google",
+      description:"search engine extrordinaire."
+    },
+    {
+      link:"http://yahoo.com",
+      title:"Yahoo",
+      description:"search engine with amazing QA site."
+    },
+    {
+      link:"http://google.com",
+      title:"Google",
+      description:"search engine extrordinaire."
+    },
+    {
+      link:"http://yahoo.com",
+      title:"Yahoo",
+      description:"search engine with amazing QA site."
+    }
+  ])
+
+  .forEach(function(item){
+    $("#archive-list").append(
+      $("<li class='expandable-archive collapsed'>" +
+            "<a shref='"+item.link+"'>" +
+              "<div class='title'>"+item.title+"</div>" +
+            "</a>" +
+            item.description +
+        "</li>"
+       )
+    );
+  });
+
+  $(".expandable-archive").click(function(){
+    var target = $(this),
+        others = $(".expandable-archive").not(target);
+    others.addClass("collapsed");
+    target.addClass("expanded");
+    others.removeClass("expanded");
+    target.removeClass("collapsed");
+  });
+
+}());
